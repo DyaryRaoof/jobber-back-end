2618abc1ba55a4a37586d3b5ae56f960d9a3afea
Dyary Raoof <dyray2020@gmail. om>
2022-06-20 23:00:00 +0300

    Add sign up user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
+
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name, :email, :password, :password_confirmation, :phone, :facebook)
+        end
+        devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :email, :password, :password_confirmation, :phone, :facebook)
+        end
+
+        devise_parameter_sanitizer.permit(:sign_in) do |u|
+        u.permit(email, :password)
+        end
+    end
+
 end
